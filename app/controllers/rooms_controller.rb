415e9da7c2ba017384cae7bb16e1c3829f8f6058
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-10-12 20:15:08 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, usr_ids: [])
+    params.require(:room).permit(:name, user_ids: [])
   end  
 end
